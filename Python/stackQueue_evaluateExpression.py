7e97a1c78f61eb1096f394693af93f7ea75cc56f
David Wong <mingtak.wong1986@gmail.com>
2023-01-12 21:45:25 -0500

    Day 28

--- Python/stackQueue_evaluateExpression.py
+++ Python/stackQueue_evaluateExpression.py
@@ -48,6 +48,7 @@ A = ["4", "13", "5", "/", "+"]
 	# @return an integer
 # 	def evalRPN(self, A):
 def evalRPN(A):
+    from collections import deque     
     def operHelper(x, y, oper):        
         if oper == '+':
             return x + y
@@ -70,57 +71,87 @@ def evalRPN(A):
         except:
             result = 0
         return result   
-    val_list = []
-    oper_list = []    
+    val_list = deque() 
+    oper_list = deque()   
     i = 0       
     while i < len(A):
-        isDigit = (A[i] in '0123456789')  
+        n_ai = len(A[i])
+        if n_ai > 1:
+            isDigit = (A[i][0] in '-0123456789')  
+        else:
+            isDigit = (A[i][0] in '0123456789')  
         # print(i, isDigit)
-        if A[i] == ' ':
-            i += 1
-            isDigit = (A[i] in '0123456789')  
-            continue
-        elif A[i] == '(':
+        # if A[i] == ' ':
+        #     i += 1
+        #     isDigit = (A[i] in '0123456789')  
+        #     continue
+        # elif A[i] == '(':
+        if A[i] == '(':
             oper_list.append(A[i])
         elif isDigit == True:
             curr_val = 0
-            isDigit = (A[i] in '0123456789')  
-            while (i < len(A) and isDigit == True):
-                print(A[i])
-                curr_val = (curr_val * 10) + int(A[i])
-                i += 1            
-                isDigit = (A[i] in '0123456789')  
-                print(A[i], isDigit)
-            val_list.append(curr_val)            
+            n_val = len(A[i])
+            j = 0
+            isNeg = False
+            if A[i][j] == '-':
+                isNeg = True
+                j += 1
+            isDigit = (A[i][j] in '0123456789')  
+            while (j < n_val and isDigit == True):
+                # print(A[i][j])
+                curr_val = (curr_val * 10) + int(A[i][j])
+                j += 1            
+                if j < n_val:
+                    isDigit = (A[i][j] in '0123456789')  
+                # print(A[i], isDigit)
+            if isNeg:
+                curr_val = -curr_val
+            val_list.append(curr_val)   
+            print('Now is', i, val_list)
         elif A[i] == ')':        
             while len(oper_list) != 0 and oper_list[-1] != '(':          
-                print('here', val_list, oper_list)    
-                y = val_list.pop()
-                x = val_list.pop()
-                oper = oper_list.pop()                
+                # print('here', val_list, oper_list)    
+                oper = oper_list.popleft()         
+                if priorty(oper) == 1: 
+                    y = val_list.popleft()
+                    x = val_list.popleft()
+                elif priorty(oper) == 2:
+                    y = val_list.pop()
+                    x = val_list.pop()    
                 val_list.append(operHelper(x, y, oper))
-                print(x, y, oper)
+                # print(x, y, oper)
             oper_list.pop()
-        else:
-            while (len(oper_list) != 0 and priorty(oper_list[-1]) >= priorty(A[i])):      
+        else:            
+            while (len(oper_list) != 0 and priorty(oper_list[-1]) > priorty(A[i])):      
                 print('there', val_list, oper_list)                  
-                y = val_list.pop()
-                x = val_list.pop()
-                oper = oper_list.pop()
-                print(x, y, oper)
-                val_list.append(operHelper(x, y, oper))
-             
-            oper_list.append(A[i])        
+                oper = oper_list.popleft()
+                if priorty(oper) == 1: 
+                    y = val_list.popleft()
+                    x = val_list.popleft()
+                elif priorty(oper) == 2:
+                    y = val_list.pop()
+                    x = val_list.pop()  
+                # print(x, y, oper)
+                val_list.append(operHelper(x, y, oper))             
+            oper_list.append(A[i])      
+            print('Oper block add', A[i], oper_list, 'at ', i)
         i += 1     
-    while len(oper_list) != 0:        
-        y = val_list.pop()
-        x = val_list.pop()
-        oper = oper_list.pop()                
+    while len(oper_list) > 0:        
+        oper = oper_list.popleft()
+        if priorty(oper) == 1: 
+            y = val_list.popleft()
+            x = val_list.popleft()
+        elif priorty(oper) == 2:
+            y = val_list.pop()
+            x = val_list.pop()               
         val_list.append(operHelper(x, y, oper))
     print(val_list)
     return val_list[-1]
 
 A =   ["2", "1", "+", "3", "*"]
 A = ["4", "13", "5", "/", "+"]
+A = [ "-1" ] #Corner Case
+A = [ "5", "1", "2", "+", "4", "*", "+", "3", "-" ]
+
 evalRPN(A)
  
